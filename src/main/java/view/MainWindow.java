cfd70d15063a815e71683baece4b56083b6edf94
Emanuel Tiradani <ematiradani@gmail.com>
2022-05-22 23:01:54 -0300

    Initial commit

---
+++ src/main/java/view/MainWindow.java
@@ -0,0 +1,5 @@
+package view;
+
+public interface MainWindow {
+    void showView();
+}
---
+++ src/main/java/view/MainWindowImpl.java
@@ -0,0 +1,10 @@
+package view;
+
+public class MainWindowImpl implements MainWindow{
+
+
+    @Override
+    public void showView() {
+
+    }
+}
