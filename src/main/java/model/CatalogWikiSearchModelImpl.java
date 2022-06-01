39aa80ade331926e40be9bd3eb93b4c21a9d008e
EmaTiradani <ematiradani@gmail.com>
2022-06-01 20:34:46 -0300

    MVP ready. Road to clean y estamos.

---
+++ src/main/java/model/CatalogWikiSearchModelImpl.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.ArrayList;
+
+public class CatalogWikiSearchModelImpl implements CatalogWikiSearchModel{
+
+
+    @Override
+    public void addListener(CatalogModelListener listener) {
+
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return null;
+    }
+
+    @Override
+    public void searchOnWiki(String title) {
+
+    }
+
+    @Override
+    public String getExtract(SearchResult searchResult) {
+        return null;
+    }
+
+    @Override
+    public String getExtract2() {
+        return null;
+    }
+
+    @Override
+    public void setSearchMode(boolean onlyIntro) {
+
+    }
+
+    @Override
+    public ArrayList<SearchResult> getPreliminaryResults() {
+        return null;
+    }
+}
